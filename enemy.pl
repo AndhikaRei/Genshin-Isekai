abf3289d78a8f79ed0127d06101e7eea09ebc6e3
hokkyss <hokkyss2@gmail.com>
2020-11-20 08:01:17 +0700

    lengkapin encounter, ngeprint stats musuh, sama menyederhanakan bbrp predikat

--- enemy.pl
+++ enemy.pl
@@ -1,21 +1,39 @@
-/* baseEnemy(X, Level, HP, Max HP, ATK, Special ATK, DEF) */
+:- dynamic(inBattleEnemy/7).
+/* diassert pas encounter */
+/* pas diserang, dia diretract & diassert */
+/* kalau udah selesai battle, di retract lagi */
+
+
+/* btw kayaknya ini kalau selesai battle, pertambahan exp nya juga belum deh */
+/* Jadi kayaknya itu diperlukan juga */
+
+/* baseEnemy(X, HP, ATK, Special ATK, DEF) */
 /* base stats enemy di level 1 */
-baseEnemy(smallSlime, 1, 75, 75, 30, 50, 5).
-baseEnemy(bigSlime, 1, 100, 100, 40, 70, 10).
-baseEnemy(recruitGoblin, 1, 125, 125, 50, 80, 10).
-baseEnemy(berserkerGoblin, 1, 200, 200, 75, 120, 15).
-baseEnemy(standardWolf, 1, 180, 180, 70, 130, 20).
-baseEnemy(direWolf, 1, 260, 260, 100, 200, 25).
+baseEnemy(smallSlime, 75, 30, 50, 5).
+baseEnemy(bigSlime, 100, 40, 70, 10).
+baseEnemy(recruitGoblin, 125, 50, 80, 10).
+baseEnemy(berserkerGoblin, 200, 75, 120, 15).
+baseEnemy(standardWolf, 180, 70, 130, 20).
+baseEnemy(direWolf, 260, 100, 200, 25).
 
-/* growthRate(X, Level, Max HP, ATK, Special ATK, DEF) */
+/* growthRate(X, Max HP, ATK, Special ATK, DEF) */
 /* enemy naik level tiap player naik level */
-growthEnemy(smallSlime, 1, 5, 7, 2).
-growthEnemy(bigSlime, 1, 10, 12, 3).
-growthEnemy(recruitGoblin, 1, 8, 10, 2).
-growthEnemy(berserkerGoblin, 1, 13, 15, 4).
-growthEnemy(standardWolf, 1, 12, 14, 3).
-growthEnemy(direWolf, 1, 15, 19, 5).
+growthEnemy(smallSlime, 5, 7, 2).
+growthEnemy(bigSlime, 10, 12, 3).
+growthEnemy(recruitGoblin, 8, 10, 2).
+growthEnemy(berserkerGoblin, 13, 15, 4).
+growthEnemy(standardWolf, 12, 14, 3).
+growthEnemy(direWolf, 15, 19, 5).
 
+statsEnemy(X) :-
+		  inBattleEnemy(X, Level, HP, MaxHP, Atk, SAtk, Def),
+		  write('Enemy : '), write(X), nl,
+		  write('Level : '), write(Level), nl,
+		  write('HP : '), write(HP), write('/'), write(MaxHP), nl,
+          write('Attack : '), write(Atk), nl,
+		  write('Special Attack : '), write(SAtk), nl,
+		  write('Defense : '), write(Def), nl.
+	
 /* boss(X, Level, HP, Max HP, ATK, Special ATK, DEF) */
 /* X adalah nama boss dengan atribut level, HP, Max HP, ATK, Special ATK, dan DEF yang sudah didefinisi */
 boss(regisvine, 10, 2000, 2000, 350, 700, 45).
--- player.pl
+++ player.pl
@@ -7,20 +7,21 @@
 :- dynamic(player/8).
 :- dynamic(playerEquipment/3).
 
-/* Job, Level, HP (and also Max HP), Attack, Defense */
-growthRate(swordsman, 1, 150, 5, 3).
-growthRate(archer, 1, 50, 8, 2).
-growthRate(sorcerer, 1, 100, 6, 3).
+/* Job, HP (and also Max HP), Attack, Defense */
+growthRate(swordsman, 150, 5, 3).
+growthRate(archer, 50, 8, 2).
+growthRate(sorcerer, 100, 6, 3).
 
 /* Base Stats di Level 1 */
-/* Job, Level, HP, MaxHP, Attack, Defense, Exp, Gold */
-baseStat(swordsman, 1, 500, 500, 30, 25, 0, 10000).
-baseStat(archer, 1, 400, 400, 50, 20, 0, 100000).
-baseStat(sorcerer, 1, 450, 450, 45, 10, 0, 100000).
+/* Job, MaxHP, Attack, Defense, Exp, Gold */
+baseStat(swordsman, 500, 30, 25, 0, 100000).
+baseStat(archer, 400, 50, 20, 0, 100000).
+baseStat(sorcerer, 450, 45, 10, 0, 100000).
 
 /* Pas milih class, assert player dengan BaseStat dari jobnya */
 /* Di main pas bagian inisialisasi (atau di init.pl juga boleh kalau mau */
 /* assertz(Player(......)) */
+
 exp(1,0,1).
 /* exp(Lv, _, Total) :- Total is Lv*Lv*lv. */
 status :- player(X, Lvl, HP, MaxHP, Att, Def, E, G),
@@ -44,10 +45,10 @@ printEqStat(X) :-
 	).
 
 levelUp(X) :- 
-	growthRate(X, L, Health, Attack, Defense),
+	growthRate(X, Health, Attack, Defense),
 	player(Job, Lvl, HP, MaxHP, Att, Def, E, G),
 	retract(player(Job, Lvl, HP, MaxHP, Att, Def, E, G)),
-	NewLvl is Lvl + L, NewHP is HP + Health, NewMax is MaxHP + Health, NewAtt is Att + Attack, NewDeff is Def + Defense,
+	NewLvl is Lvl + 1, NewHP is HP + Health, NewMax is MaxHP + Health, NewAtt is Att + Attack, NewDeff is Def + Defense,
 	assertz(player(Job, NewLvl, NewHP, NewMax, NewAtt, NewDeff, E, G)).
 
 addExp(X) :-
