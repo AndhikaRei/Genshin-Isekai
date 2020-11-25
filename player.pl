700c5d2fdd3acf47e9f07490ad91fa02bb64e4f1
hokkyss <hokkyss2@gmail.com>
2020-11-25 13:04:13 +0700

    lupa edit apa :)

--- player.pl
+++ player.pl
@@ -10,7 +10,7 @@ growthRate(sorcerer, 150, 23, 9).
 /* Base Stats di Level 1 */
 /* Job, MaxHP, Attack, Defense, Exp, Gold */
 baseStat(swordsman, 1000, 10000000, 70, 0, 20000).
-baseStat(archer, 500, 75, 40, 0, 20000).
+baseStat(archer, 500, 75, 30, 0, 20000).
 baseStat(sorcerer, 200, 95, 10, 0, 20000).
 
 /* Pas milih class, assert player dengan BaseStat dari jobnya */
